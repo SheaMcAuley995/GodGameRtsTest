5ff3934d391b411eb1823e315429493d7cacbdce
Shea McAuley <s179049@students.aie.edu.au>
2018-08-29 12:47:04 -0700

    rts camera work

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,31 +4,57 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
+
+    public Vector3 targetPos;
     public float panSpeed = 20f;
     public float panBorderThickness = 10f;
+    public Vector2 panLimit;
+    public float scrollSpeed = 20f;
+    public float minY = 20f;
+    public float maxY = 140f;
+
+    public LayerMask collisionLayer;
 
 	// Update is called once per frame
 	void Update () {
 
-        Vector3 pos = transform.position;
-
 		if(Input.GetKey(KeyCode.W) || Input.mousePosition.y >= Screen.height - panBorderThickness)
         {
-            pos.z += panSpeed * Time.deltaTime;
+            targetPos.z += panSpeed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.S) || Input.mousePosition.y <= panBorderThickness)
         {
-            pos.z -= panSpeed * Time.deltaTime;
+            targetPos.z -= panSpeed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.D) || Input.mousePosition.x >= Screen.width - panBorderThickness)
         {
-            pos.x += panSpeed * Time.deltaTime;
+            targetPos.x += panSpeed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.A) || Input.mousePosition.x <= panBorderThickness)
         {
-            pos.x -= panSpeed * Time.deltaTime;
+            targetPos.x -= panSpeed * Time.deltaTime;
         }
 
-        transform.position = pos;
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        targetPos.y -= scroll * scrollSpeed * 100f * Time.deltaTime;
+
+
+
+        targetPos.x = Mathf.Clamp(targetPos.x, -panLimit.x, panLimit.x);
+        targetPos.y = Mathf.Clamp(targetPos.y, minY, maxY);
+        targetPos.z = Mathf.Clamp(targetPos.z, -panLimit.y, panLimit.y);
+
+        groundCorrection();
+        transform.position = targetPos;
 	}
+
+
+    private void groundCorrection()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, -transform.up,out hit, minY))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
 }
